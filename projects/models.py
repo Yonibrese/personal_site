14405e12765b7559b9b365ae4124b1584e8614ec
jonathan <yonibrese@gmail.com>
2020-04-22 11:21:25 +0300

    added backend

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    screen_shot = models.ImageField(upload_to='projects/images/%Y/%m/%d',null=True, blank=True)
+    url = models.URLField(max_length=200)
+    description = models.TextField(null=True)
