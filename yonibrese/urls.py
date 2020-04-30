8f978809c1e65a11f22abd3e11a9fbe759aa8996
jonathan <yonibrese@gmail.com>
2020-04-30 07:54:27 +0300

    more styling added about path

--- yonibrese/urls.py
+++ yonibrese/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('web/', include('projects.urls')),
     path('wood/', include('woodwork.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
