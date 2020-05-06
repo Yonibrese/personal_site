4a1314ace4baae51248c377863ab59432651dd81
jonathan <yonibrese@gmail.com>
2020-05-06 14:26:02 +0300

    contact page styling

--- yonibrese/urls.py
+++ yonibrese/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('web/', include('projects.urls')),
     path('wood/', include('woodwork.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
