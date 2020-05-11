4c00579120efcbf4f7d9bd6f07dbe2d0985c3fcf
jonathan <yonibrese@gmail.com>
2020-05-11 13:27:57 +0300

    added the comment form

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<int:id>', views.comment, name='comment')
 ]
