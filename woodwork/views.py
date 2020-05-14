29291a5e41f9899a798287849ec4ba75fefd6144
jonathan <yonibrese@gmail.com>
2020-05-14 13:59:38 +0300

    post deploy and edit

--- woodwork/views.py
+++ woodwork/views.py
@@ -3,5 +3,5 @@ from . import models
 
 
 def wood_gallery(request):
-    works = models.WoodObjects.objects.all().order_by('-date_added')
+    works = models.WoodObjects.objects.all().order_by('date_added')
     return render(request, 'woodwork/gallery.html', {'works': works})
